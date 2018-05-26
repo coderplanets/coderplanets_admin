459ef2b0dc1917216e527b8f2029f4b6f5c05a8a
mydearxym <mydearxym@qq.com>
2018-05-26 20:15:38 +0800

    eslint fix

--- containers/CommunitiesContent/IndexContent.js
+++ containers/CommunitiesContent/IndexContent.js
@@ -145,7 +145,10 @@ class IndexContent extends React.Component {
   }
 
   render() {
-    const { data, restProps: { communitiesLoading } } = this.props
+    const {
+      data,
+      restProps: { communitiesLoading },
+    } = this.props
 
     return (
       <div>
