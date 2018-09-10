3566b80f4bbff3541a95cf6acbc76eae976ba535
mydearxym <mydearxym@qq.com>
2018-09-10 14:07:39 +0800

    build: clean up

--- config/endpoint.js
+++ config/endpoint.js
@@ -3,8 +3,7 @@
 const getGraphQLEndpoint = () => {
   switch (process.env.GOAL) {
     case 'production':
-      return 'http://localhost:4001/graphiql'
-    /* return 'http://api.coderplanets.com/graphiql' */
+      return 'http://api.coderplanets.com/graphiql'
 
     case 'dev':
       return 'http://devapi.coderplanets.com/graphiql'
@@ -17,5 +16,7 @@ const getGraphQLEndpoint = () => {
   }
 }
 export const GRAPHQL_ENDPOINT = getGraphQLEndpoint()
-export const ISSUE_ADDR = 'https://github.com/mydearxym/mastani_web/issues/new'
+export const GITHUB_ADDR = 'https://github.com/coderplanets/coderplanets_admin'
+export const GITHUB_ME = 'https://github.com/mydearxym'
+export const ISSUE_ADDR = `${GITHUB_ADDR}/issues/new`
 export const MENTION_USER_ADDR = 'https://coderplanets.com/users/'
--- deploy/production/loader.sh
+++ deploy/production/loader.sh
@@ -2,7 +2,7 @@
 
 cd /root/web/
 
-pm2-runtime start npm --name "coderplanets_web" -- run launch &
+pm2-runtime start npm --name "coderplanets_admin" -- run launch &
 
 while true
 do
